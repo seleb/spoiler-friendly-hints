792f6ca9b90f58a0980f4d8df15b52642574961c
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2026-06-05 19:46:28 -0400

    implement

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,1 +1,55 @@
+import { describe, expect, test, vi } from 'vitest';
+import { convert } from ".";
 
+vi.mock(import('../package.json'), (original) => ({
+	...original,
+	default: {
+		...original.default,
+		name: 'vitest',
+		version: 'TEST',
+	},
+}));
+
+describe('convert', () => {
+	test('default values', async (context) => {
+		const input = `
+foo
+	bar
+	baz
+		!!! alice
+			bob
+`;
+		await expect(convert(input)).toMatchFileSnapshot(__filename.replace(/.ts$/, ` - ${context.task.name}.snapshot.html`));
+	});
+	test('without preamble', async (context) => {
+		const input = `
+foo
+	bar
+	baz
+		!!! alice
+			bob
+`;
+		await expect(convert(input, {
+			preamble: false,
+		})).toMatchFileSnapshot(__filename.replace(/.ts$/, ` - ${context.task.name}.snapshot.html`));
+	});
+	test('with all options', async (context) => {
+		const input = `
+foo
+  bar
+  baz
+    !!! alice
+      bob
+`;
+		await expect(convert(input, {
+			preamble: true,
+			title: 'My Title',
+			preambleUrl: 'https://test.com',
+			colorText: '#F00',
+			colorBg: '#0F0',
+			colorAccent: '#00F',
+			indent: /(^(?:\s{2})*)(.*$)/,
+			highlight: /baz/,
+		})).toMatchFileSnapshot(__filename.replace(/.ts$/, ` - ${context.task.name}.snapshot.html`));
+	});
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,214 @@
+import * as crypto from 'crypto';
+import pkg from '../package.json';
 
+function logX(num: number, base: number): number {
+	return Math.log10(num) / Math.log10(base);
+}
+
+export function convert(text: string, {
+	preamble = true,
+	title = '',
+	preambleUrl = '',
+	colorText = '#FFF',
+	colorBg = '#000',
+	colorAccent = '#FFC',
+	indent = /(^\t*)(.*$)/,
+	highlight = /!!!/,
+}: {
+	/** if `true`, output includes a short preamble */
+	preamble?: boolean;
+	/** if provided, title referenced in document head and preamble */
+	title?: string;
+	/** if provided, link wrapping title in preamble */
+	preambleUrl?: string;
+	colorText?: string;
+	colorBg?: string;
+	colorAccent?: string;
+	/** regex used to split text */
+	indent?: RegExp;
+	/** regex used to determine highlighted lines */
+	highlight?: RegExp;
+} = {}): string {
+	const lines = text
+		.trim()
+		.split(/[\r\n]+/)
+		.filter((i) => i.trim());
+
+	type Hint = { summary: string; detail: Hint[] };
+	const hints: Hint[] = [{ summary: 'hints', detail: [] }];
+	const stack = [hints[0]];
+
+	const tail = <T>(a: T[]): T => a[a.length - 1];
+
+	// eslint-disable-next-line no-restricted-syntax
+	for (const l of lines) {
+		const [, tabs, hint] = l.split(indent);
+		const depth = tabs.length + 1;
+		if (depth > stack.length) {
+			stack.push(tail(tail(stack).detail));
+		}
+		while (depth < stack.length) {
+			stack.pop();
+		}
+		tail(stack).detail.push({ summary: hint, detail: [] });
+	}
+	const ids = new Set();
+
+	function getId(input: string): string {
+		let extra = 0;
+		let id = '';
+		while (!id && extra < 100) {
+			id = `h${crypto
+				.createHash('shake256', {
+					outputLength: Math.ceil(logX(lines.length, 8)),
+				})
+				.update(`${input}${extra}`)
+				.digest('hex')}`;
+			if (ids.has(id)) {
+				id = '';
+				++extra;
+			}
+		}
+		ids.add(id);
+		return id;
+	}
+
+	/* eslint-disable indent */
+	const renderHint = (hint: (typeof hints)[number], depth: number): string => {
+		const t = '\t'.repeat(depth);
+		return hint.detail.length > 0
+			? `
+${t}<details id="${getId(`${hint.summary}.${depth}`)}">
+${t}	<summary>${hint.summary}${highlight?.exec(hint.summary) ? '<span class="highlight"></span>' : ''}</summary>
+${t}	<ul>${hint.detail
+				.map(
+					(d) => `
+${t}		<li>${renderHint(d, depth + 3)}
+${t}		</li>`,
+				)
+				.join('')}
+${t}	</ul>
+${t}</details>`
+		: `
+${t}${hint.summary}`;
+	};
+	/* eslint-enable indent */
+
+	return `
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<title>${[title, 'Hints'].filter((i) => i).join(' | ')}</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+	<meta name="theme-color" content="${colorBg}">
+	<meta name="generator" content="${pkg.name} v${pkg.version}" />
+
+	<style>
+		html {
+			--bg: ${colorBg};
+			--text: ${colorText};
+			--accent: ${colorAccent};
+			background: var(--bg);
+			color: var(--text);
+			font-family: 'font', 'Courier New', Courier, monospace;
+			scrollbar-gutter: stable;
+		}
+
+		ul {
+			margin: 0;
+			padding: 0;
+			list-style: none;
+			margin-inline-start: 0.5em;
+		}
+
+		li {
+			font-style: italic;
+		}
+
+		details {
+			font-style: initial;
+			border: solid 1px transparent;
+		}
+
+		details[open] {
+			border-left-color: var(--accent);
+			border-top-color: var(--accent);
+		}
+
+		summary {
+			cursor: pointer;
+			color: var(--accent);
+			padding: 0.25rem;
+		}
+
+		summary:has(> .highlight) {
+			background-color: rgba(from var(--text) r g b / 0.5);
+			color: var(--bg);
+		}
+
+		a {
+			color: var(--accent);
+		}
+
+		summary:hover,
+		summary:focus {
+			color: inherit;
+			background-color: rgba(from var(--text) r g b / 0.2);
+		}
+
+		details i,
+		details q {
+			font-weight: bold;
+		}
+	</style>
+</head>
+
+<body>
+	<h1>${[title, 'Hints'].filter((i) => i).join(' | ')}</h1>
+
+	${preamble ? `<p>This is a list of hints${title ? ` for ${preambleUrl ? `<a href="${preambleUrl}">${title}</a>` : title}` : ''}. These hints start out vague and get more specific the deeper you expand in order to try to help you get unstuck while trying to avoid spoiling full solutions.</p>` : ''}
+
+	${renderHint(hints[0], 1)}
+
+	<script>
+		let openingA = false;
+		let openingB = false;
+		function openHintsFromHash(event) {
+			if (openingB) return;
+			openingA = true;
+			const state = (event ? new URL(event.newURL).hash : location.hash).split('|').filter(i => i && i !== '#').reduce((acc, i) => {
+				acc[i] = true;
+				return acc;
+			}, {});
+			document.querySelectorAll('details').forEach(elDetails => {
+				if (elDetails.open !== state[elDetails]) elDetails.open = state[elDetails.id];
+			});
+			openingA = false;
+		}
+		window.addEventListener('hashchange', openHintsFromHash);
+		openHintsFromHash();
+
+		document.querySelectorAll('details').forEach(elDetails => {
+			elDetails.addEventListener('toggle', (event) => {
+				if (openingA) return;
+				openingB = true;
+				const hashOld = location.hash;
+				const hashNewArr = hashOld.split('|').filter(i => i && i !== '#' && i !== event.currentTarget.id);
+				if (event.currentTarget.open) {
+					hashNewArr.push(event.currentTarget.id);
+				}
+				hashNewArr.unshift('#');
+				const hashNew = hashNewArr.sort().join('|');
+				if (hashOld !== hashNew) location.hash = hashNew;
+				openingB = false;
+				event.stopPropagation();
+			});
+		});
+	</script>
+</body>
+
+</html>
+`.trim();
+}
