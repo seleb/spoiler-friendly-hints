44b6444e6e1764a1da50eacb024ea521b56157ae
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2026-06-05 19:44:47 -0400

    vite types

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { resolve } from 'node:path';
+import dts from 'unplugin-dts/vite';
 import { defineConfig } from 'vite';
 import pkg from './package.json';
 
 export default defineConfig({
+	plugins: [
+		dts({
+			exclude: '**.test.ts',
+		}),
+	],
 	build: {
 		lib: {
 			entry: resolve(import.meta.dirname, './src/index.ts'),
